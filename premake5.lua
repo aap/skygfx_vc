3f21ca232597e8e98476dfb487030d70bf928bf4
ThirteenAG <thirteenag@gmail.com>
2017-01-07 16:19:20 +0300

    premake adjustments

--- premake5.lua
+++ premake5.lua
@@ -18,10 +18,10 @@ workspace "skygfx_vc"
    links { "rwd3d9.lib" }
    
    prebuildcommands {
-	"for /R \"../shaders/ps/\" %%f in (*.hlsl) do \"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T ps_2_0 /nologo /E main /Fo ../resources/release/%%~nf.cso %%f",
-	"for /R \"../shaders/vs/\" %%f in (*.hlsl) do \"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T vs_2_0 /nologo /E main /Fo ../resources/release/%%~nf.cso %%f",
-	"\"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T vs_2_0 /nologo /E mainVS /Fo ../resources/release/vehiclePass1VS.cso ../shaders/pvs/vehiclePass1VS.hlsl",
-	"\"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T vs_2_0 /nologo /E mainVS /Fo ../resources/release/vehiclePass2VS.cso ../shaders/pvs/vehiclePass2VS.hlsl"
+	"for /R \"../shaders/ps/\" %%f in (*.hlsl) do \"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T ps_2_0 /nologo /E main /Fo ../resources/cso/%%~nf.cso %%f",
+	"for /R \"../shaders/vs/\" %%f in (*.hlsl) do \"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T vs_2_0 /nologo /E main /Fo ../resources/cso/%%~nf.cso %%f",
+	"\"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T vs_2_0 /nologo /E mainVS /Fo ../resources/cso/vehiclePass1VS.cso ../shaders/pvs/vehiclePass1VS.hlsl",
+	"\"%DXSDK_DIR%/Utilities/bin/x86/fxc.exe\" /T vs_2_0 /nologo /E mainVS /Fo ../resources/cso/vehiclePass2VS.cso ../shaders/pvs/vehiclePass2VS.hlsl"
    }
 	  
 project "skygfx_vc"
--- shaders/shaders.bat
+++ shaders/shaders.bat
@@ -1,5 +1,5 @@
-for /R "./ps/" %%f in (*.hlsl) do "%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T ps_2_0 /nologo /E main /Fo ../resources/release/%%~nf.cso %%f
-for /R "./vs/" %%f in (*.hlsl) do "%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T vs_2_0 /nologo /E main /Fo ../resources/release/%%~nf.cso %%f
+for /R "./ps/" %%f in (*.hlsl) do "%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T ps_2_0 /nologo /E main /Fo ../resources/cso/%%~nf.cso %%f
+for /R "./vs/" %%f in (*.hlsl) do "%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T vs_2_0 /nologo /E main /Fo ../resources/cso/%%~nf.cso %%f
 
-"%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T vs_2_0 /nologo /E mainVS /Fo ../resources/release/vehiclePass1VS.cso ./pvs/vehiclePass2VS.hlsl
-"%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T vs_2_0 /nologo /E mainVS /Fo ../resources/release/vehiclePass2VS.cso ./pvs/vehiclePass2VS.hlsl
+"%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T vs_2_0 /nologo /E mainVS /Fo ../resources/cso/vehiclePass1VS.cso ./pvs/vehiclePass2VS.hlsl
+"%DXSDK_DIR%\Utilities\bin\x86\fxc.exe" /T vs_2_0 /nologo /E mainVS /Fo ../resources/cso/vehiclePass2VS.cso ./pvs/vehiclePass2VS.hlsl
